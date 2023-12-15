60788d4f26ef11a6af053f1023dacff6b0391df6
Supasan Suklim <supasan.game@gmail.com>
2023-12-15 18:40:03 +0700

    Init

---
+++ src/main/java/com/example/springbootdockerdev/SpringBootDockerDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootdockerdev/SpringBootDockerDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
